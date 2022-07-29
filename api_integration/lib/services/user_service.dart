adef3738bcf830af5378b9ffa455d8715475633d
Abdullah <abdullahsoomro245@gmail.com>
2022-07-29 14:57:53 +0500

    Task Complete

---
+++ api_integration/lib/services/user_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../model/user_model.dart';
+
+getUsers() async {
+  var response =
+      await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+  var jsonData = jsonDecode(response.body);
+
+  List<UserModel> users = [];
+  for (var i in jsonData) {
+    UserModel user =
+        UserModel(id: i['id'], name: i['name'], username: i['username']);
+    users.add(user);
+  }
+  return users;
+}
